f57498b9b3ae77287b016da7414b523c0e68015a
jrodriguez-1 <jrod1527@gmail.com>
2024-10-01 10:43:56 -0500

    ready for deployment

--- my-portfolio/next.config.mjs
+++ my-portfolio/next.config.mjs
@@ -1,10 +1,6 @@
-const withPWA = require('next-pwa')({
-  dest: 'public',
-  disable: process.env.NODE_ENV === 'development'
-});
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  optimizeFonts: false,
   reactStrictMode: true,
   images: {
     deviceSizes: [640, 750, 828, 1080, 1200, 1920, 2048, 3840],
@@ -18,4 +14,4 @@ const nextConfig = {
   },
 };
 
-module.exports = withPWA(nextConfig);
+export default nextConfig;
--- my-portfolio/tailwind.config.ts
+++ my-portfolio/tailwind.config.ts
@@ -3,42 +3,37 @@ import colors from "tailwindcss/colors";
 
 const config: Config = {
   content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./utils/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}"
   ],
   theme: {
     extend: {
       colors: {
         'black-rgba': 'rgba(0, 0, 0, 0.8)',
         primary: {
-          500: '#2563eb', // your primary color
+          ...colors.purple,
+          500: '#2563eb', // your custom primary color
           600: '#1e40af', // darker shade for hover
         },
+        secondary: colors.pink,
       },
       backgroundImage: {
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+        "gradient-conic": "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      },
+      animation: {
+        fade: 'fadeIn 1.5s ease-in-out',
+      },
+      keyframes: {
+        fadeIn: {
+          from: { opacity: '0' },
+          to: { opacity: '1' },
+        },
       },
     },
-    colors: {
-      ...colors,
-      primary: colors.purple,
-      secondary: colors.pink,
-    },
   },
-
-  animation: {
-    fade: 'fadeIn 1.5s ease-in-out',
-    },
-    keyframes: {
-    fadeIn: {
-    from: { opacity: 0 },
-    to: { opacity: 1 },
-    },
-    },
-
   plugins: [],
 };
+
 export default config;
